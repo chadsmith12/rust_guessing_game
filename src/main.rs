66ebf8ade3763fcc2929b8a6471b9a9ecde67df1
Chad Smith <smith.chad12@gmail.com>
2023-04-28 22:21:15 -0500

    add inputting in number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// bring in the standard io library
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // define a variable that can be mutatable
+    let mut guess = String::new();
+
+    // attempt to read in line from the standard input and store it in the guess
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
