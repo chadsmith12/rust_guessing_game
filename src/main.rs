5173f24e29249d2638dd0a2fc6af3821939aaa5e
Chad Smith <smith.chad12@gmail.com>
2023-04-28 23:02:42 -0500

    complete guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
 // bring in the standard io library
 use std::io;
+use std::cmp::Ordering;
+// bring in the Rng trait from rand to get methods for random numbers
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // define a variable that can be mutatable
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    // attempt to read in line from the standard input and store it in the guess
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // define a variable that can be mutatable
+        let mut guess = String::new();
+    
+        // attempt to read in line from the standard input and store it in the guess
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        // we are using shadowing here on guess so we can reuse the variable since we are just converting it
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>  {
+                println!("You win!!");
+                break;
+            }
+        }
+    }
 }
